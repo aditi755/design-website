88b063728348d40dbd59bcec8172baa12fc668e6
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-02-21 19:36:02 +0530

    edited

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
   return (
     <div className="services">
         <div className="services-title">
-            WHERE MOST DESIGNERS GO WRONG AND YOU DONT HAVE TO
+            WHERE MOST DESIGNERS GO WRONG 
         </div>
 
         <div className="services-card">
